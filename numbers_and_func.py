7004308b7f54197fd7a27a6aed961be9a7657ea6
lester wee <lester.wee.68@gmail.com>
2023-10-11 23:30:56 +0800

    learn enumerate function

--- numbers_and_func.py
+++ numbers_and_func.py
@@ -2,7 +2,7 @@
 # These variables are used to keep track of the two most recent Fibonacci numbers.
 a, b = 0, 1
 while a < 1000:
-    print(a, end=',')
+    # print(a, end=',')
     # a, b = b, a+b: After printing the current value of a, the values of a and b are updated. a is set to the current value of b,
     # and b is set to the sum of the old values of a and b. This is a common technique used to generate the next number in the Fibonacci sequence.
     # a becomes the previous Fibonacci number, and b becomes the next Fibonacci number in the sequence.
@@ -18,4 +18,18 @@ def fib(n):    # write Fibonacci series up to n
     print()
 
 # Now call the function we just defined:
-fib(2000)
+# fib(2000)
+
+my_list = ["eat", "sleep", "repeat"]
+word = "geek"
+
+# creating enumerate objects
+obj1 = enumerate(my_list)
+obj2 = enumerate(word)
+
+print ("Return type:", type(obj1))
+print (list(enumerate(my_list)))
+print (list(enumerate(word)))
+
+# changing start index to 2 from 0
+print (list(enumerate(word, 2)))
