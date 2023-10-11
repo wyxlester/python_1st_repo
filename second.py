9a5093e1fa42f4bd308cc4d0d359d107d6baf341
lester wee <lester.wee.68@gmail.com>
2023-10-11 17:40:22 +0800

    return Fibonacci series up to n

--- second.py
+++ second.py
@@ -13,3 +13,15 @@ def fib(n):    # write Fibonacci series up to n
 
 # Now call the function we just defined:
 fib(2000)
+
+def fib2(n):  # return Fibonacci series up to n
+    """Return a list containing the Fibonacci series up to n."""
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)    # see below
+        a, b = b, a+b
+    return result
+
+f100 = fib2(500)    # call it
+print(f100)         # write the result
